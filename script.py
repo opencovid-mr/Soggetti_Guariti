f6b04933a1ebdac56f4f967286a55bd83cb823d9
opencovid-mr <63929979+opencovid-mr@users.noreply.github.com>
2022-04-12 11:58:16 +0200

    Update script.py

--- script.py
+++ script.py
@@ -12,3 +12,15 @@ csv_file = open(filename, 'wb')
 csv_file.write(req.content)
 csv_file.close()
 
+
+url='https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/platea-second-booster.csv'
+
+filename='data/platea-second-booster-'+datetime.today().strftime('%Y-%m-%d')+'.csv'
+
+req = requests.get(url)
+csv_file = open(filename, 'wb')
+
+csv_file.write(req.content)
+csv_file.close()
+
+
